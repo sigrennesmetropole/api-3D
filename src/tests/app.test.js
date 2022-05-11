e2f2e50fa55eea7fc07c2b6084328cd851433f86
CRENN <yoann.crenn@cgi.com>
2022-05-11 15:37:52 +0200

    ajout des premiers tests

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,11 +1,22 @@
-const app = require("../index")
-const supertest = require("supertest")
-const request = supertest(app)
+const ExpressServer = require('../expressServer');
+const supertest = require("supertest");
+const config = require('../config');
+const fs = require('fs');
+const path = require('path');
+const request = supertest( new ExpressServer(config.URL_PORT, config.OPENAPI_YAML).app);
 
 describe("/hello", () => {
     it("should return a response", async () => {
         const response = await request.get("/hello")
         expect(response.status).toBe(200)
-        expect(response.text).toBe(`Hello World. path: ${this.openApiPath}`);
+        expect(response.text).toBe(`Hello World. path: ${ config.OPENAPI_YAML}`);
     })
-})
+});
+
+describe("/openapi", () => {
+    it("should return a response", async () => {
+        const response = await request.get("/openapi")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(fs.readFileSync(path.join(__dirname, '..\\api', 'openapi.yaml'), 'utf8'));
+    })
+});

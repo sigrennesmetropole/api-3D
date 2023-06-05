3b6f74552dc2d7b2d2e204b06d5aeb7fb312d0a9
Cédric Briand <ce.briand@rennesmetropole.fr>
2023-06-05 11:48:10 +0200

    Test suivi audience matomo 2

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ const launchServer = async () => {
 
 
 <!-- Matomo -->
-<script>
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -31,7 +30,6 @@ const launchServer = async () => {
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
 <!-- End Matomo Code -->
 
 launchServer().catch(e => logger.error(e));

c36c473f50ce8a862b9bd93e06eac55395ca7682
Cédric Briand <ce.briand@rennesmetropole.fr>
2023-06-05 11:53:39 +0200

    Revert "Test suivi audience matomo"
    This reverts commit c3c981cf58e555408b2c1d9d6cb6ad990cdde54d.

--- src/index.js
+++ src/index.js
@@ -17,21 +17,4 @@ const launchServer = async () => {
   }
 };
 
-
-<!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//app.sig.rennesmetropole.fr/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '8']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
-
 launchServer().catch(e => logger.error(e));
